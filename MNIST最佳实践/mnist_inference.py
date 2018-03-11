af12b6c18365f0d91d03d736ff3a3cf52315774f
gugug <734093894@qq.com>
2018-03-11 19:53:37 +0800

    调整训练神经网络参数的偏置biases

--- MNIST最佳实践/mnist_inference.py
+++ MNIST最佳实践/mnist_inference.py
@@ -20,12 +20,12 @@ def get_weight_variable(shape, regularizer):
 def inference(input_tensor, regularizer):
     with tf.variable_scope('layer1'):
         weights = get_weight_variable([INPUT_NODE, LAYER1_NODE], regularizer)
-        biases = tf.get_variable("biases", [LAYER1_NODE], initializer=tf.constant_initializer(0.0))
+        biases = tf.get_variable("biases", [LAYER1_NODE], initializer=tf.constant_initializer(0.1))
         layer1 = tf.nn.relu(tf.matmul(input_tensor, weights) + biases)
 
     with tf.variable_scope('layer2'):
         weights = get_weight_variable([LAYER1_NODE, OUTPUT_NODE], regularizer)
-        biases = tf.get_variable("biases", [OUTPUT_NODE], initializer=tf.constant_initializer(0.0))
+        biases = tf.get_variable("biases", [OUTPUT_NODE], initializer=tf.constant_initializer(0.1))
         layer2 = tf.matmul(layer1, weights) + biases
 
     return layer2
