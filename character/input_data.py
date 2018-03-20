390f1a4ae66bd2de5a086d36bc5bf770a122a292
gugug <734093894@qq.com>
2018-03-20 22:50:00 +0800

    load_data_label_combine整合特征

--- character/input_data.py
+++ character/input_data.py
@@ -3,12 +3,13 @@
 import os
 
 import numpy as np
+from crawl_textmind_data import input_textmind_data
 
 
 def load_data_label(base_model_dir):
-    train_vec_filename = os.path.join(base_model_dir, "doc2vec_train_vec_dm.npy")
+    train_vec_filename = os.path.join(base_model_dir, "../svm_baseline_baobao/doc2vec_train_vec_dbow.npy")
     train_label_filename = os.path.join(base_model_dir, 'doc2vec_train_label_dm.npy')
-    test_vec_filename = os.path.join(base_model_dir, 'doc2vec_test_vec_dm.npy')
+    test_vec_filename = os.path.join(base_model_dir, '../svm_baseline_baobao/doc2vec_test_vec_dbow.npy')
     test_label_filename = os.path.join(base_model_dir, 'doc2vec_test_label_dm.npy')
 
     X_train = np.load(train_vec_filename)
@@ -22,8 +23,24 @@ def load_data_label(base_model_dir):
     return X_train, Y_train, X_test, Y_test
 
 
-# if __name__ == '__main__':
-    # X_train, Y_train, X_test, Y_test = load_data_label('')
-    # print(X_test)
-    # print(Y_test[:, 0])  # 取第i列
-    # print np.where(X_test > 0, 1, 0)  # 对每一个元素判断 大于0为1,否则为0
+def load_data_label_combine():
+    """
+    整合文心特征和doc2vec特征
+    :return:
+    """
+    X_train, Y_train, X_test, Y_test = load_data_label('')
+    X1_train, Y1_train, X1_test, Y1_test = input_textmind_data.load_textmind_data_label('../crawl_textmind_data')
+    X_train_all = np.hstack((X_train, X1_train))
+    X_test_all = np.hstack((X_test, X1_test))
+    return X_train_all, Y_train, X_test_all, Y_test
+
+
+if __name__ == '__main__':
+    X_train, Y_train, X_test, Y_test = load_data_label('')
+    print(X_test)
+    print(Y_test)
+    X_train, Y_train, X1_test, Y1_test = input_textmind_data.load_textmind_data_label('../crawl_textmind_data')
+    print(X1_test)
+    print(Y1_test)
+    X_test_all = np.hstack((X_test, X1_test))
+    print(X_test_all.shape)
