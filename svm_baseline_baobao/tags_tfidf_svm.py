d7092b5031577c0d53a7a58267fd4682a0f8b102
gugug <734093894@qq.com>
2018-03-29 17:05:12 +0800

    tfidf不降维

--- svm_baseline_baobao/tags_tfidf_svm.py
+++ svm_baseline_baobao/tags_tfidf_svm.py
@@ -3,15 +3,15 @@
 
 from __future__ import division
 
+import codecs
+import os
+
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn import svm
-import codecs
 from sklearn.externals import joblib
-
 from numpy import *
 from gensim import models, corpora
 import numpy as np
-import os
 
 
 class user_predict:
@@ -42,21 +42,16 @@ class user_predict:
         f = codecs.open(doc)
         temp = f.readlines()
         f.close()
-        print len(temp)
-
         for i in range(len(temp)):
             temp[i] = temp[i].split(" ")
             user_name = temp[i][0]
             tags = temp[i][1:6]
             query = temp[i][6:]
             query = " ".join(query).strip().replace("\n", "")
-            # query = re.sub("[+\.\!\/_,$%^*(+\"\']+|[+——！，。？、~@#￥%……&*（）]+".decode("utf8"), "".decode("utf8"),query)
 
             list_total.append(query)
             list_label.append(tags)
 
-        print list_total.__len__()
-        print list_label.__len__()
         # 字符串标签转化为int类型
         list_tag = []
         for line in list_label:
@@ -70,7 +65,7 @@ class user_predict:
         return list_total, list_tag
 
     # -------------------------prepare lsi svd -----------------------
-    def prepare_lsi(self, doc):
+    def prepare(self, doc):
 
         # 给训练集用的，返回文本和对应的标签
         list_total, list_tag = self.load_data(doc)
@@ -79,84 +74,21 @@ class user_predict:
         texts = [[word for word in document.lower().split()]
                  for document in list_total]
 
-        # train dictionary done
         dictionary = corpora.Dictionary(texts)  # 生成词典
-        # print dictionary.token2id
-        # 用TFIDF的方法计算词频,sublinear_tf 表示学习率
         tfv = TfidfVectorizer(min_df=1, max_df=0.95, sublinear_tf=True, stop_words=stop_word)
-        # 对文本中所有的用户对应的所有的评论里面的单词进行ＴＦＩＤＦ的计算，找出每个词对应的tfidf值
         X_sp = tfv.fit_transform(list_total)
         corpus = [dictionary.doc2bow(text) for text in texts]
-        # train model done
         tfidf_model = models.TfidfModel(corpus)
-        joblib.dump(tfidf_model, "tfidf_model.model")
+        joblib.dump(tfidf_model, "tfidf_model_notlsi.model")
+        joblib.dump(dictionary, "tfidf_dictionary_notlsi.dict")
+        return tfidf_model, dictionary, X_sp
 
-        corpus_tfidf = tfidf_model[corpus]
-
-        lsi_model = models.LsiModel(corpus_tfidf, id2word=dictionary, num_topics=200)
-
-        joblib.dump(dictionary, "tfidf_dictionary.dict")
-        print "训练集lsi -----"
-        joblib.dump(lsi_model, "tfidf_lsi.model")
-
-        return tfidf_model, dictionary
-
-    def train_lsi(self, doc):
-
-        if not (os.path.exists("tfidf_model.model")):
-
-            print "prepare model"
-            # load train model
-            tfidf_model, dictionary = self.prepare_lsi(doc)
-
-            # load data
-            list_total, list_tag = self.load_data(doc)
-            stop_word = []
-            texts = [[word for word in document.lower().split() if word not in stop_word]
-                     for document in list_total]
-
-            corpus = [dictionary.doc2bow(text) for text in texts]
-
-        else:
-            print "use model"
-            # load train valid text
-            tfidf_model = joblib.load("tfidf_model.model")
-            dictionary = joblib.load("tfidf_dictionary.dict")
-
-            # load data
-            list_total, list_tag = self.load_data(doc)
-            stop_word = []
-            texts = [[word for word in document.lower().split() if word not in stop_word]
-                     for document in list_total]
-
-            corpus = [dictionary.doc2bow(text) for text in texts]
-
-        lsi_model = joblib.load("tfidf_lsi.model")
-        corpus_tfidf = tfidf_model[corpus]
-
-        list_side = []
-
-        corpus_lsi = lsi_model[corpus_tfidf]
-        nodes = list(corpus_lsi)
-
-        for i in range(len(nodes)):
-            list_d = []
-            for j in range(200):
-                # print nodes[i][j]
-                list_d.append(nodes[i][j][1])
-            list_side.append(list_d)
-
-        print "lsi 矩阵构建完成----------------"
-
-        return list_total, list_tag, list_side
-
-    # -----------------------write vec--------------------
-    def write_d2v(self, X_sp, doc_name):
-
-        file_name = "tfidf_" + doc_name + ".npy"
-
-        np.save(file_name, X_sp)
-        print "*****************write done over *****************"
+    def train(self, doc):
+        list_total, list_tag = self.load_data(doc)
+        tfv = TfidfVectorizer(min_df=1, max_df=0.95, sublinear_tf=True, stop_words=[])
+        X_sp = tfv.fit_transform(list_total)
+        print X_sp.shape
+        return list_total, list_tag, X_sp
 
     # ------------------------my mean count------------------
     def mymean(self, list_predict_score, array_test):
@@ -167,60 +99,33 @@ class user_predict:
 
     # ------------------------------begin to predict------------
     def predict(self):
-
-        train_list_total, train_list_tag, train_list_side = self.train_lsi(self.train_document)
+        train_list_total, train_list_tag, train_list_side = self.train(self.train_document)
         print "train model done -------------------"
-
-        text_list_total, text_list_tag, text_list_side = self.train_lsi(self.text_document)
+        text_list_total, text_list_tag, text_list_side = self.train(self.text_document)
         print "text model done  -------------------"
-
-        TR = train_list_total.__len__()
-        TE = text_list_total.__len__()
-        n = 5
-
-        train_list_side = mat(train_list_side)
-        text_list_side = mat(text_list_side)
-
-        X_train = train_list_side[:TR]
-        y_train = train_list_tag[:TR]
+        X_train = train_list_side
+        y_train = train_list_tag
         y_train = np.array(y_train)
-
         print "train shape :---------------------"
         print X_train.shape
-
-        X_text = text_list_side[:TE]
-        y_text = text_list_tag[:TE]
+        X_text = text_list_side
+        y_text = text_list_tag
         y_text = np.array(y_text)
-
         print "text shape :---------------------"
         print X_text.shape
-
-        # kfold折叠交叉验证
-        list_myAcc = []
         true_acc = 0
-
         for i in range(5):
             list_train_tags = []
             list_test_tags = []
             print "第" + str(i) + "个分类器训练"
-
-            # first build train tag
             for line in y_train:
                 list_train_tags.append(line[i])
-
-            # first build text tag
             for line in y_text:
                 list_test_tags.append(line[i])
-
             clf = svm.SVC(probability=True)
-
             clf = svm.SVC(kernel='linear', probability=True)
-
-            # 逻辑回归训练模型
             clf.fit(X_train, list_train_tags)
-            # 用模型预测
             y_pred_te = clf.predict_proba(X_text)
-
             print np.argmax(y_pred_te, axis=1)
             print "**" * 50
             print list_test_tags
@@ -228,10 +133,8 @@ class user_predict:
             # #获取准确的个数
             print self.myAcc(list_test_tags, y_pred_te)
             true_acc += self.myAcc(list_test_tags, y_pred_te)
-
         print "true acc numbers: " + str(true_acc)
-
-        print "LSI + 支持向量机　准确率平均值为: "
+        print "不使用LSI降维 + 支持向量机　准确率平均值为: "
         print self.mymean(true_acc, X_text)
 
 
